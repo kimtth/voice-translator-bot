e2ec690e86840e98dc39d079fc1bb1f7d78ba25d
haha <haha>
2020-08-06 18:16:02 -0700

    update screenshot

--- ref/react-memo-app-master/src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as InputMemoContainer } from './InputMemoContainer';
-export { default as MemoBoxListContainer } from './MemoBoxListContainer';
-export { default as FullMemoContainer } from './FullMemoContainer';
-export { default as ControlledDimmed } from './ControlledDimmed';
