db09e8dc74353e76450ee2cfff22eb58940bf644
kim <iyai@naver.com>
2020-10-27 23:47:23 +0900

    Create Constants.js

---
+++ front-react/src/components/App/Constants.js
@@ -0,0 +1,7 @@
+export const API_URL='http://localhost:4000'
+export const SPEECH_SUBSCRIPTION_KEY=''
+export const SPEECH_SERVICE_REGION=''
+export const SPEECH_RECOGNITION_LANGUAGE=''
+export const TRANSLATOR_TEXT_SUBSCRIPTION_KEY=''
+export const TRANSLATOR_TEXT_REGION_AKA_LOCATION=''
+export const TRANSLATOR_TEXT_ENDPOINT=''
